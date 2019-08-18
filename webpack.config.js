d7b85a289923159bcae04d7458a24109127ca432
Sam Hodak <slhodak@gmail.com>
2019-08-17 22:52:01 -0700

    fix css modules

--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,16 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-        options: {
-          modules: true
-        }
+        use: ['style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[path][name]__[local]--[hash:base64:5]'
+              }
+            }
+          }
+        ]
       }
     ]
   }
