099091848fd93ef99f6f8314ad1439f56c7d1359
Sam Hodak <slhodak@gmail.com>
2019-08-17 23:49:38 -0700

    replace controlled component with on-enter input

--- src/index.js
+++ src/index.js
@@ -6,33 +6,25 @@ class Graph extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      notWantNotHave: '',
-      wantHave: '',
-      wantNotHave: '',
-      haveNotWant: ''
+      notWantNotHave: [],
+      wantHave: [],
+      wantNotHave: [],
+      haveNotWant: []
     }
-    this.handleInputs = this.handleInputs.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
-  handleInputs(event, quadrant) {
-    if (quadrant === 'notWantNotHave') {
+
+  handleKeyDown(event, quadrant) {
+    if(event.keyCode === 13 && event.target.value) {
+      this.state[quadrant].push(event.target.value);
       this.setState({
-        notWantNotHave: event.target.value
-      });
-    } else if (quadrant === 'wantHave') {
-      this.setState({
-        wantHave: event.target.value
-      });
-    } else if (quadrant === 'wantNotHave') {
-      this.setState({
-        wantNotHave: event.target.value
-      });
-    } else if (quadrant === 'haveNotWant') {
-      this.setState({
-        haveNotWant: event.target.value
+        [quadrant]: this.state[quadrant]
+      }, () => {
+        event.target.value = '';
       });
     }
-    console.log(this.state);
   }
+
   render() {
     return (
       <div className={styles.all}>
@@ -41,10 +33,10 @@ class Graph extends React.Component {
           <div className={styles.quadrants}>
             <h2 className={styles.topHeader}>Have To Do -></h2>
             <h2 className={styles.leftHeader}>Want To Do /\</h2>
-            <Quadrant quadrant="wantHave" handleChange={this.handleInputs} />
-            <Quadrant quadrant="wantNotHave" handleChange={this.handleInputs} />
-            <Quadrant quadrant="haveNotWant" handleChange={this.handleInputs} />
-            <Quadrant quadrant="notWantNotHave" handleChange={this.handleInputs} />
+            <Quadrant quadrant="wantHave" handleKeyDown={this.handleKeyDown} />
+            <Quadrant quadrant="wantNotHave" handleKeyDown={this.handleKeyDown} />
+            <Quadrant quadrant="haveNotWant" handleKeyDown={this.handleKeyDown} />
+            <Quadrant quadrant="notWantNotHave" handleKeyDown={this.handleKeyDown} />
           </div>
           <List notWantNotHave={this.state.notWantNotHave} 
             wantHave={this.state.wantHave} 
@@ -61,14 +53,27 @@ class Quadrant extends React.Component {
   constructor(props) {
     super(props)
     this.handleChange = this.handleChange.bind(this);
+    this.attachKeyDownHandler = this.attachKeyDownHandler.bind(this);
+    this.detachKeyDownHandler = this.detachKeyDownHandler.bind(this);
   }
+
   handleChange(event) {
     this.props.handleChange(event, this.props.quadrant);
-  };
+  }
+
+  attachKeyDownHandler(event) {
+    window.onkeydown = (event) => {
+      this.props.handleKeyDown(event, this.props.quadrant);
+    };
+  }
+  detachKeyDownHandler(event) {
+    window.onkeydown = null;
+  }
+
   render() {
     return (
       <div className={styles[this.props.quadrant]}>
-        <div className={styles.taskArea}>
+        <div className={styles.taskArea} onFocus={this.attachKeyDownHandler} onBlur={this.detachKeyDownHandler}>
           <input className={styles.taskInput} onChange={this.handleChange} placeholder={this.props.quadrant} />
           <div className={styles.taskPool}></div>
         </div>
@@ -83,18 +88,18 @@ class List extends React.Component {
       <div className={styles.list}>
         <h2>The List:</h2>
         <ol>
-          {this.props.haveNotWant && this.props.haveNotWant.split(',').map(function(item) {
-              return <li>{item.trim()}</li>;
-          })}
-          {this.props.wantHave && this.props.wantHave.split(',').map(function(item) {
-              return <li>{item.trim()}</li>;
-          })}
-          {this.props.wantNotHave && this.props.wantNotHave.split(',').map(function(item) {
-              return <li>{item.trim()}</li>;
-          })}
-          {this.props.notWantNotHave && this.props.notWantNotHave.split(',').map(function(item) {
-              return <li>{item.trim()}</li>;
-          })}
+          {this.props.haveNotWant.length ? this.props.haveNotWant.map(function(item) {
+              return <li>{item}</li>;
+          }) : null}
+          {this.props.wantHave.length ? this.props.wantHave.map(function(item) {
+              return <li>{item}</li>;
+          }) : null}
+          {this.props.wantNotHave.length ? this.props.wantNotHave.map(function(item) {
+              return <li>{item}</li>;
+          }) : null}
+          {this.props.notWantNotHave.length ? this.props.notWantNotHave.map(function(item) {
+              return <li>{item}</li>;
+          }) : null}
         </ol>
       </div>
     );
