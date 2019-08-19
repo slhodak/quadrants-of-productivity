4ba334928437dedeac559de9f7c4adc38b842886
Sam Hodak <slhodak@gmail.com>
2019-08-18 19:53:15 -0700

    include checkbox svg

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import styles from '../styles.css';
+import checkbox from '../svg/checkbox.js';
 
 class ListItem extends React.Component {
   render() {
+    const { item, quadrant } = this.props;
     return (
-      <div className={styles.ListItem}></div>
+      <li>
+        <div className={styles.ListItem}>
+          <span>{item}</span>
+          <span onClick={() => {
+            this.props.delete(quadrant, item)
+          }}>{checkbox}</span>
+        </div>
+      </li>
     );
   }
 }
