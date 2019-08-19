2b0aade4f8d02a6ff7c942e95f65d88f458aab71
Sam Hodak <slhodak@gmail.com>
2019-08-18 20:42:35 -0700

    Add arrows to instruction box

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from '../styles.css';
+import arrowUp from '../svg/arrowUp.js';
+import arrowDown from '../svg/arrowDown.js';
 
 export default (props) => {
   return (
@@ -7,7 +9,7 @@ export default (props) => {
       <button className={styles.toggle} onClick={props.toggle}>{props.open ? 'Close' : 'Open'}</button>
       {props.open
         ? <div className={styles.instructions}>
-            <p>Instructions</p>
+            <p>Instructions<span className={styles.arrow}>{arrowUp}</span></p>
             <p>Things you are thinking of doing probably exist along two axes:</p>
             <ol>
               <li>How much you want to do them</li>
@@ -15,7 +17,7 @@ export default (props) => {
             </ol>
             <p>Add your tasks into the appropriate text areas, and listen to the list.</p>
           </div>
-        : <div className={styles.instructionsEmpty}>Instructions </div>}
+        : <div className={styles.instructionsEmpty}>Instructions<span className={styles.arrow}>{arrowDown}</span></div>}
     </div>
   );
 };
