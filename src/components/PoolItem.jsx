2f81b2a5ed2566b30de3a42c7d121b4965da21f8
Sam Hodak <slhodak@gmail.com>
2019-08-18 19:43:57 -0700

    destructure quadrant

--- src/components/PoolItem.jsx
+++ src/components/PoolItem.jsx
@@ -3,13 +3,13 @@ import styles from '../styles.css';
 
 class PoolItem extends React.Component {
   render() {
-    const { item } = this.props;
+    const { item, quadrant } = this.props;
     return (
       <div className={styles.poolItem}>
         <span className={styles.item}>{item}</span>
         <span className={styles.delete} 
               onClick={() => {
-                this.props.delete(this.props.quadrant, item)
+                this.props.delete(quadrant, item)
               }}>
           x
         </span>
